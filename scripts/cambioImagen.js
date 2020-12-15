b8afb347015cf9ce231a87f32712ead6f71946de
DIEGODOPAZO <57042869+DIEGODOPAZO@users.noreply.github.com>
2020-12-15 11:39:55 +0100

    Add files via upload

---
+++ scripts/cambioImagen.js
@@ -0,0 +1,18 @@
+﻿<script> 
+var img = {src:"", id:""};
+
+function cambiar(imagen) {
+    if(img.src=="" && img.id==""){
+        img.src=imagen.src;
+        img.id=imagen.id;
+    }
+    else{ 
+        var aux = imagen.src;
+
+        document.getElementById(imagen.id).src = img.src;
+        document.getElementById(img.id).src = aux;
+
+        img.src = ""; img.id="";
+    }
+} 
+</script>
